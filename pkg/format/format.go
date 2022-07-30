95023feebda82f9011351a49275a0a400e572ed8
Maddie Schipper <me@maddiesch.com>
2022-07-29 19:07:39 -0500

    Start Formatting

--- pkg/format/format.go
+++ pkg/format/format.go
@@ -1,15 +1,42 @@
 package format
 
-import "github.com/maddiesch/marble/pkg/ast"
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/maddiesch/marble/pkg/ast"
+)
 
 func Format(node ast.Node) (string, error) {
-	if f, ok := node.(Formattable); ok {
-		return f.Format()
+	f := &Formatter{}
+	if err := f.Format(node); err != nil {
+		return "", err
 	}
-
-	panic("format.Format - not implemented")
+	return f.String(), nil
 }
 
 type Formattable interface {
-	Format() (string, error)
+	Format(*Formatter) error
 }
+
+type Formatter struct {
+	buffer bytes.Buffer
+}
+
+func (f *Formatter) Format(node ast.Node) error {
+	if node, ok := node.(Formattable); ok {
+		return node.Format(f)
+	}
+	return fmt.Errorf("node %v does not implement Formattable interface", node)
+}
+
+func (f *Formatter) Write(p []byte) (int, error) {
+	return f.buffer.Write(p)
+}
+
+func (f *Formatter) String() string {
+	return f.buffer.String()
+}
+
+var _ io.Writer = (*Formatter)(nil)
